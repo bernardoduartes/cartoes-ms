b8fd703988fbf612b927dd21e4e7c9c8d8b88cbd
Bernardo Duarte <bernardoduartes@gmail.com>
2022-09-18 22:54:54 -0300

    init

---
+++ src/main/java/com/shizuca/cartoesms/CartoesMsApplication.java
@@ -0,0 +1,13 @@
+package com.shizuca.cartoesms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartoesMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartoesMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shizuca/cartoesms/CartoesMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shizuca.cartoesms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartoesMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
